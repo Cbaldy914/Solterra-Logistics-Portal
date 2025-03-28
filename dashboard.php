f3b29a2949ad5cb814f7908c2f432296199f6369
Connor Baldy <cbaldy@solterrasol.com>
2025-03-28 11:03:19 -0400

    removed debug code

--- dashboard.php
+++ dashboard.php
@@ -19,9 +19,7 @@ if (!$conn) {
 }
 
 // Enable error reporting for debugging (remove in production)
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 // 1) Determine which account(s) the user belongs to
 $user_id = $_SESSION['user_id'];
