ec7f61d6fc014486b7ed81636f982025310d059b
Connor Baldy <cbaldy@solterrasol.com>
2025-05-14 10:30:23 -0400

    Before project_overview tabs updating

--- warehouse_info.php
+++ warehouse_info.php
@@ -608,7 +608,7 @@ if ($conn) {
          $back_link = "manage_warehouses.php";
      }
      ?>
-     <a href="<?php echo $back_link; ?>" class="back-icon">
+     <a href="<?php echo $back_link; ?>" class="back-icon" style="margin:20px;">
          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="fill: currentColor; width: 24px; height: 24px;">
              <path d="M10 19c-.39 0-.78-.15-1.06-.44L3.5 13.06a1.5 1.5 0 010-2.12l5.44-5.5a1.5 1.5 0 012.12 2.12L7.12 11H19a1.5 1.5 0 010 3H7.12l3.44 3.44a1.5 1.5 0 01-1.06 2.56z"/>
          </svg>
