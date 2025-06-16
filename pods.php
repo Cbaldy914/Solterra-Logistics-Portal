03499fe4d5f048847a2e9d8cb31cd6acfe221621
Connor Baldy <cbaldy@solterrasol.com>
2025-06-16 09:40:26 -0400

    project_cost_details looks great

--- pods.php
+++ pods.php
@@ -96,7 +96,7 @@ $stmt->close();
     <div class="breadcrumb">
         <a href="<?php echo isset($_SESSION['role']) && ($_SESSION['role'] === 'admin' || $_SESSION['role'] === 'global_admin') ? 'admin_dashboard.php' : 'dashboard.php'; ?>">Dashboard</a>
         <span class="separator">&raquo;</span>
-        <a href="project_overview.php?id=<?php echo $project_id; ?>">Project Overview</a>
+        <a href="project_overview.php?project_id=<?php echo $project_id; ?>">Project Overview</a>
         <span class="separator">&raquo;</span>
         <span>PODs</span>
     </div>
