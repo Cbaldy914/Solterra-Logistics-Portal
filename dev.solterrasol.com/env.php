c860cd903d2097b76e05c6664db44c5abb7b2cc1
Connor Baldy <cbaldy@solterrasol.com>
2025-10-29 09:57:44 -0400

    dev environment set up

---
+++ dev.solterrasol.com/env.php
@@ -0,0 +1,18 @@
+<?php
+/*  Dev secrets for dev.solterrasol.com (do not commit to public repos)  */
+
+// ▼ Database
+putenv('DB_HOST=127.0.0.1');
+putenv('DB_NAME=solterra_dev');
+putenv('DB_USER=dev_user');
+putenv('DB_PASS=Solterra1!');
+
+// ▼ Environment flag
+putenv('APP_ENV=dev');
+
+// ▼ Optional non-prod API keys
+// putenv('GOOGLE_MAPS_API_KEY=');
+// putenv('OPENAI_API_KEY=');
+?>
+
+
---
+++ dev.solterrasol.com/login.php
@@ -0,0 +1,5 @@
+<?php
+header('Location: /Solterra-Logistics-Portal/login.php');
+exit();
+
+
