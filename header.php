561a1f473696f17fd35f43f796cfd152700c15d3
Connor Baldy <cbaldy@solterrasol.com>
2025-12-17 10:47:10 -0500

    Implement user invitation feature and refactor invite user modal

--- header.php
+++ header.php
@@ -270,12 +270,8 @@ $unread_notifications = unread_notification_count($_SESSION['user_id']);
             </div>
           </li>
 
-          <li class="slp-item slp-has-dropdown" role="none">
-            <a href="#" class="slp-link" role="menuitem" aria-haspopup="true" aria-expanded="false">Users</a>
-            <div class="slp-submenu" role="menu">
-              <a href="admin_management" role="menuitem">Admin Management</a>
-              <a href="invite_user" role="menuitem">Invite User</a>
-            </div>
+          <li class="slp-item" role="none">
+            <a href="admin_management" class="slp-link" role="menuitem">Users & Accounts</a>
           </li>
 
           <li class="slp-item slp-has-dropdown" role="none">
